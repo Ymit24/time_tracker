63cea424995b202482b79d8781dbc379ceb89c15
Christian Smith <christianryansmith@gmail.com>
2023-06-18 23:02:31 -0700

    added a bunch of features

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,265 @@
-import Image from 'next/image'
+'use client'
+import { Backdrop, Box, Button, Stack, CircularProgress, Container, CssBaseline, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, Typography, createTheme, TextField } from '@mui/material'
+import { ChangeEvent, useEffect, useState } from 'react';
+
+function SimpleBackdrop() {
+    const [open, setOpen] = useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleOpen}>Show backdrop</Button>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+                onClick={handleClose}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </div>
+    );
+}
+
+function createData(
+    id: number,
+    name: string,
+    start: Date,
+    stop?: Date
+) {
+    return { id, name, start, stop, isStopped: false };
+}
+
+function getDurationStrFor(duration: number): string {
+    const seconds = duration / 1000;
+    const minutes = seconds / 60;
+    const hours = Math.floor(minutes / 60);
+    let durationStr = "";
+    const minutesVal = Math.floor(minutes % 60);
+    const secondsVal = Math.floor(seconds % 60);
+
+    if (hours > 0) { durationStr += `${hours} hrs ` }
+    if (minutesVal > 0) { durationStr += `${minutesVal} mins ` }
+    if (secondsVal >= 0) { durationStr += `${secondsVal} secs` }
+
+    return durationStr;
+}
+
+function getDurationStrForDates(start: Date, stop?: Date): string {
+    const duration = (stop || new Date()).valueOf() - start.valueOf();
+    return getDurationStrFor(duration);
+}
+
+type TimeEntry = {
+    id: number,
+    name: string,
+    start: Date,
+    stop?: Date,
+    isStopped: boolean,
+};
+
+type TimeSheet = {
+    entries: TimeEntry[]
+};
+
+const seedRows = [
+    createData(0, "Ticket #3", new Date(2023, 5, 18, 7), new Date(2023, 5, 18, 8)),
+    createData(1, "Meeting", new Date(2023, 5, 18, 8), new Date(2023, 5, 18, 8, 30)),
+    createData(2, "Ticket #1", new Date(2023, 5, 18, 8, 30), new Date(2023, 5, 18, 9)),
+    createData(3, "Ticket #7", new Date(2023, 5, 18, 9)),
+];
+
+function BasicTable({ timeSheet, stopEntryFunc, newEntryFrom }: { timeSheet: TimeSheet, stopEntryFunc: (id: number) => void, newEntryFrom: (entry: TimeEntry) => void }) {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Start</TableCell>
+                        <TableCell align="right">Stop</TableCell>
+                        <TableCell align="right">Duration</TableCell>
+                        <TableCell align="right">Controls</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {timeSheet.entries.map((row) => (
+                        <TableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.start?.toLocaleString()}</TableCell>
+                            <TableCell align="right">{row.isStopped ? row.stop?.toLocaleString() : "In Progress"}</TableCell>
+                            <TableCell align="right">{getDurationStrForDates(row.start, row.stop)}</TableCell>
+                            <TableCell align="right">{!row.isStopped ?
+                                <Button onClick={() => stopEntryFunc(row.id)}>Stop</Button>
+                                : <Button onClick={() => newEntryFrom(row)}>New</Button>
+                            }</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+function getDurationOf(entry: TimeEntry): number {
+    return (entry.stop || new Date()).valueOf() - entry.start.valueOf();
+}
+
+function CondensedTable({ timeSheet }: { timeSheet: TimeSheet }) {
+    const rows: { name: string, durationStr: string, durationRaw: number, entries: number }[] = [];
+
+    timeSheet.entries.forEach((entry) => {
+        const index = rows.findIndex((row) => row.name == entry.name);
+        if (index == -1) {
+            rows.push({
+                name: entry.name,
+                durationStr: getDurationStrForDates(entry.start, entry.stop),
+                durationRaw: getDurationOf(entry),
+                entries: 1
+            });
+        } else {
+            rows[index].durationRaw += getDurationOf(entry);
+            rows[index].durationStr = getDurationStrFor(rows[index].durationRaw);
+            rows[index].entries++;
+        }
+    });
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align='left'>Name</TableCell>
+                        <TableCell align='right'>Duration</TableCell>
+                        <TableCell align='right'>Entries</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell align='left'>{row.name}</TableCell>
+                            <TableCell align='right'>{row.durationStr}</TableCell>
+                            <TableCell align='right'>{row.entries.toString()}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    let originalExistingTimesheet = JSON.parse(localStorage.getItem('timesheet') || '{entries: []}') as TimeSheet;
+    const existingTimesheet = {
+        entries: originalExistingTimesheet.entries.map((entry) => ({
+            ...entry,
+            start: new Date(entry.start),
+            stop: new Date(entry.stop || 0),
+        }))
+    };
+    const [timesheet, setTimesheet] = useState<TimeSheet>(existingTimesheet);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [newEntryTitle, setNewEntryTitle] = useState("");
+    const onChangeNewEntryTitle = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setNewEntryTitle(e.target.value);
+    };
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const createNewEntry = () => {
+        setTimesheet({
+            entries: [
+                ...timesheet.entries,
+                createData(timesheet.entries.length, newEntryTitle, new Date())
+            ]
+        });
+        setNewEntryTitle("");
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const stopEntry = (entryId: number) => {
+        const entry = timesheet.entries.find(x => x.id == entryId);
+        if (!entry) return;
+        setTimesheet({
+            entries: [
+                ...timesheet.entries.filter(x => x.id != entryId),
+                {
+                    ...createData(entryId, entry.name, entry.start, new Date()), isStopped: true
+                }
+            ]
+        });
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    const newEntryFrom = (entry: TimeEntry) => {
+        setTimesheet({
+            entries: [
+                ...timesheet.entries,
+                createData(timesheet.entries.length, entry.name, new Date())
+            ]
+        });
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimesheet({
+                entries: timesheet.entries.map((entry) => {
+                    if (entry.isStopped) { return entry; }
+                    else return { ...entry, stop: new Date() };
+                })
+            });
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [timesheet.entries]);
+
+    useEffect(() => {
+        const toSave = {
+            entries: timesheet.entries.map((entry) => ({
+                ...entry,
+                start: entry.start.valueOf(),
+                stop: entry.stop?.valueOf(),
+            }))
+        };
+        localStorage.setItem('timesheet', JSON.stringify(toSave));
+    }, [timesheet]);
+
+    const showOutput = () => { };
+
+    type activeViews = 'entries' | 'condensed';
+    const [activeView, setActiveView] = useState<activeViews>('entries');
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Container maxWidth='md' sx={{ marginY: '20px' }}>
+                <Stack spacing='1rem'>
+                    <Typography variant='h3'>Time Tracker</Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <Stack direction='row' spacing='12px'>
+                            <TextField value={newEntryTitle} onChange={onChangeNewEntryTitle} label='Name' variant='outlined' />
+                            <Button variant="outlined" onClick={createNewEntry}>new</Button>
+                        </Stack>
+                        {activeView == 'entries' && <Button variant="outlined" color='secondary' onClick={() => setActiveView('condensed')}>show condensed</Button>}
+                        {activeView == 'condensed' && <Button variant="outlined" color='secondary' onClick={() => setActiveView('entries')}>show entries</Button>}
+                    </Box>
+                    <Typography variant='h4'>{activeView == 'entries' ? "Entries" : "Condensed"}</Typography>
+                    {activeView == 'entries' && <BasicTable timeSheet={timesheet} stopEntryFunc={stopEntry} newEntryFrom={newEntryFrom} />}
+                    {activeView == 'condensed' && <CondensedTable timeSheet={timesheet} />}
+                </Stack>
+            </Container>
+        </ThemeProvider >
+    )
 }
