4f3b749c6c61ceb94b84fa3d0fddc77c05e8455c
Christian Smith <christianryansmith@gmail.com>
2023-07-09 09:36:03 -0700

    feat: work on layout

--- src/app/timesheets/layout.tsx
+++ src/app/timesheets/layout.tsx
@@ -1,5 +1,30 @@
-import { ReactNode } from "react";
+'use client'
+import Link from "@components/Link";
+import { IconButton, Paper, Stack, Typography } from "@mui/material";
+import { ReactNode } from "react"
+import AddIcon from '@mui/icons-material/Add';
+import Grid from '@mui/material/Unstable_Grid2';
 
-export default function TimesheetsLayout({ children }: { children: ReactNode }) {
-    return <>{children}</>;
+
+export interface Props {
+    children: ReactNode;
+}
+
+export default function TimeSheetLayout({ children }: Props) {
+    return (
+        <Grid container gap={2}>
+            <Grid xs={2}>
+                <Paper>
+                    <Stack gap={2} sx={{ alignItems: 'start', padding: '1.5rem' }}>
+                        <Typography variant='h6'>Timesheets</Typography>
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((it, index) => <Link key={index} href={`/timesheets/${index}`}>Timesheet {index}</Link>)}
+                        <IconButton sx={{ alignSelf: 'center' }}><AddIcon color='success' /></IconButton>
+                    </Stack>
+                </Paper>
+            </Grid>
+            <Grid xs={9}>
+                {children}
+            </Grid>
+        </Grid>
+    );
 }
